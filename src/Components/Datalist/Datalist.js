d978b629ce432f9eaf57d949bae2deab3399f5aa
Saran-thangavel <tgsaran6@gmail.com>
2022-07-05 15:28:33 +0530

    july 5 commit

--- src/Components/Datalist/Datalist.js
+++ src/Components/Datalist/Datalist.js
@@ -53,7 +53,7 @@ function Datalist() {
                   width: "18rem",
                   marginTop: "5%",
                   marginLeft: "14%",
-                  backgroundImage: "linear-gradient(#7474bf,#348ac7)",
+                  backgroundImage: "linear-gradient(#8BC6EC,#9599E2)",
                   boxShadow:
                     "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
                 }}
